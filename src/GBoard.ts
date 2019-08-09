0f1a28a167ce69d7767f495fe56c41af626b64ad
wangqiaoqiaogithub <wangqiaot1128@qq.com>
2019-08-09 14:48:10 +0800

    :pencil: update to GraffitiBoard Details

--- src/GBoard.ts
+++ src/GBoard.ts
@@ -18,64 +18,50 @@ export class GBoard {
   }
   clear: string
   download: string
-  constructor(name: string, config: GBoardApi) {
-    this.name = name
+  constructor(config: GBoardApi) {
     this.lineWidth = config.lineWidth
-    this.eraser = config.eraser
+    this.eraser = this.utilbasename.typeof(config.eraser)
     this.eraserAttr = config.eraserAttr
     this.eraserAttr.naturename = config.eraserAttr.naturename
     this.eraserAttr.elementname = config.eraserAttr.elementname
-    this.pen = config.pen
+    this.pen = this.utilbasename.typeof(config.pen)
     this.penAttr = config.penAttr
     this.penAttr.naturename = config.penAttr.naturename
     this.penAttr.elementname = config.penAttr.elementname
-    this.clear = config.clear
+    this.clear = this.utilbasename.typeof(config.clear)
     this.download = config.download
   }
+  public name = config.name
   public utilbasename: any = new utilbase.Util()
   public canvas: any = this.utilbasename.typeof(this.name)
   public context: any = this.canvas.getContext('2d')
   public eraserEnabled: boolean = true
-  private drawCricle(x1: number, y1: number, x2: number, y2: number) {
-    this.context.beginPath()
-    this.context.moveTo(x1, y1)
-    this.context.lineWidth = this.lineWidth
-    this.context.lineTo(x2, y2)
-    this.context.stroke()
-    this.context.closePath()
-  }
-  private drawLine(x: number, y: number, radius: number, newytwo: number) {
-    this.context.beginPath()
-    this.context.arc(x, y, radius, 0, Math.PI * 2)
-    this.context.fill()
-  }
   private eraserEvent() {
     let eraser = this.utilbasename.typeof(this.eraser)
     let eraserAttr = this.eraserAttr
-    let pen = this.penAttr
+    let pen = this.utilbasename.typeof(this.pen)
     this.utilbasename.addEvent(eraser, 'click', () => {
       let eAttrnaturename = this.eraserAttr.naturename
       let eAttrelementname = this.eraserAttr.elementname
       let penAnaturename = this.penAttr.naturename
       let penAelementname = this.penAttr.elementname
-
       this.eraserEnabled = true
       this.utilbasename.addAttr(eraser, eAttrnaturename, eAttrelementname)
       this.utilbasename.removeAttr(pen, penAnaturename, penAelementname)
     })
   }
   private penEvent() {
+    let pen = this.utilbasename.typeof(this.pen)
     let eraser = this.utilbasename.typeof(this.eraser)
     let eraserAttr = this.eraserAttr
-    let pen = this.utilbasename.typeof(this.pen)
     this.utilbasename.addEvent(pen, 'click', () => {
-      let eAttrnaturename = this.eraserAttr.naturename
-      let eAttrelementname = this.eraserAttr.elementname
       let penAnaturename = this.penAttr.naturename
       let penAelementname = this.penAttr.elementname
+      let eAttrnaturename = this.eraserAttr.naturename
+      let eAttrelementname = this.eraserAttr.elementname
       this.eraserEnabled = false
       this.utilbasename.addAttr(pen, penAnaturename, penAelementname)
-      this.utilbasename.removeAttr(eraser, penAnaturename, eAttrelementname)
+      this.utilbasename.removeAttr(eraser, eAttrnaturename, eAttrelementname)
     })
   }
   private clearEvent() {
@@ -96,7 +82,7 @@ export class GBoard {
   }
   public autoCanvasSize() {
     this.canvassize()
-    this.utilbasename.addEvent(Window, 'resize', () => {
+    this.utilbasename.addEvent(window, 'resize', () => {
       this.canvassize()
     })
   }
@@ -104,6 +90,19 @@ export class GBoard {
     this.eraserEvent()
     this.penEvent()
     this.clearEvent()
+  }
+  public drawCricle(x1: number, y1: number, x2: number, y2: number) {
+    this.context.beginPath()
+    this.context.moveTo(x1, y1)
+    this.context.lineWidth = this.lineWidth
+    this.context.lineTo(x2, y2)
+    this.context.stroke()
+    this.context.closePath()
+  }
+  public drawLine(x: number, y: number, radius: number, newytwo: number) {
+    this.context.beginPath()
+    this.context.arc(x, y, radius, 0, Math.PI * 2)
+    this.context.fill()
   }
   public listentoUser(config: GBoardApi): void {
     let using: boolean = false
--- src/base/index.ts
+++ src/base/index.ts
@@ -15,10 +15,10 @@ export namespace utilbase {
      *通过Util基类中的addEvent方法兼容addEventListener和attachEvent并提供该接口
      */
     public addEvent(element: any, type: string, fn: any) {
-      if ((document as any).addEventListener || (window as any).addEventListener) {
+      if ((document as any).addEventListener) {
         element.addEventListener(type, fn, false)
         return element
-      } else if ((document as any).attachEvent || (window as any).attachEvent) {
+      } else if ((document as any).attachEvent) {
         var bound: any = function() {
           return fn.apply(element, arguments) //不能使用箭头函数表达式(特性)
           /**以下为箭头函数表达式带来的以下主要特性
