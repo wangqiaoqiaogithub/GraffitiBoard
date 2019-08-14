9c54557e6e2c278f5deeb7193442386e161653ba
wangqiaoqiaogithub <wangqiaot1128@qq.com>
2019-08-14 13:44:42 +0800

    :pencil: update to Typescript Methods

--- src/base/index.ts
+++ src/base/index.ts
@@ -11,6 +11,7 @@ export namespace utilbase {
     public typeof(element: string) {
       return typeof element === 'string' ? document.querySelector(element) : element
     }
+    public extend() {}
     /**
      *通过Util基类中的addEvent方法兼容addEventListener和attachEvent并提供该接口
      */
