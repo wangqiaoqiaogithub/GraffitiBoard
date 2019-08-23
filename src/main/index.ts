d7eea864d303b209bdb52eb4fd941ac710c4ef5f
wangqiaoqiaogithub <wangqiaot1128@qq.com>
2019-08-24 07:53:16 +0800

    :running: updatate to main modules Methods

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,18 +2,19 @@ import { utilbase } from './base/index'
 import { GBoardApi } from './types'
 export namespace Mainpoint {
   export class mianMethods {
-    name: string
-    lineWidth: number
+    undoname: string
     constructor(config: GBoardApi) {
       this.name = this.utilbasename.typeof()
       this.lineWidth = this.config.lineWidth
     }
+    public canvasHistory = []
+    public step = -1
     public utilbasename: any = new utilbase.Util()
-    public brushscope(name: string, lineWidth: number) {
-      this.utilbasename.addEvent(name, 'mouseup', () => {})
-    }
     public cancel() {
-      this.utilbasename(name, 'click', () => {})
+      let stepnumber = this.step
+      if (stepnumber > 0) {
+      } else {
+      }
     }
   }
 }
